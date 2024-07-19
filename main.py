3c0638243a579785fd6249806fc3b9ffaea69285
inal <balkarov05@gmail.com>
2024-07-19 16:29:56 +0300

    add pydantic models

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from fastapi import HTTPException
+from pydantic import BaseModel, validator, field_validator
+from pydantic.networks import EmailStr
 from sqlalchemy import Column, String, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
@@ -5,6 +8,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 import settings
 import uuid
+import re
 
 engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
 
@@ -22,3 +26,58 @@ class User(Base):
     surname = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     is_active = Column(Boolean(), default=True)
+
+
+class UserDAL:
+    def __init__(self, db_session: AsyncSession):
+        self.db_session = db_session
+
+    async def create_user(
+            self, name: str, surname: str, email: str
+    ) -> User:
+        new_user = User(
+            name=name,
+            surname=surname,
+            email=email,
+        )
+        self.db_session.add(new_user)
+        await self.db_session.flush()
+        return new_user
+
+
+LETTER_MATCH_PATTERN = re.compile(r'^[а-яА-Яa-zA-Z\-]+$]')
+
+
+class TunedModel(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class ShowUser(TunedModel):
+    user_id: uuid.UUID
+    name: str
+    surname: str
+    email: EmailStr
+    is_active: bool
+
+
+class CreateUser(BaseModel):
+    name: str
+    surname: str
+    email: EmailStr
+
+    @field_validator("name")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Name must contain only letters"
+            )
+        return value
+
+    @field_validator("surname")
+    def validate_name(cls, value):
+        if not LETTER_MATCH_PATTERN.match(value):
+            raise HTTPException(
+                status_code=422, detail="Surname must contain only letters"
+            )
+        return value
